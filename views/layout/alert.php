fbe500313fe411d640e60d9b7809aff5748e6367
abdelilah ouslimane <abdelilah.ouslimane3740@gmail.com>
2025-12-04 06:16:11 +0100

    feat(php):Ajouter function alert_seccuss pour affichier le message success

--- views/layout/alert.php
+++ views/layout/alert.php
@@ -1,4 +1,5 @@
 <?php
+//function alert
 function alert_success($message){
     echo "<div class='bg-green-100 border mb-2 border-green-400 text-green-700 px-4 py-3 rounded relative' role='alert'>
             <strong class='font-bold'>Succès !</strong>
