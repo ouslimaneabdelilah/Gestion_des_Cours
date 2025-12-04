e044b14612cee1b807545ee5723a0c1341d24599
abdelilah ouslimane <abdelilah.ouslimane3740@gmail.com>
2025-12-04 14:00:12 +0100

    fix: Refactor message alert display in courses list

--- views/courses/courses_list.php
+++ views/courses/courses_list.php
@@ -4,12 +4,10 @@ require_once '../../database/config/config.php';
 require_once '../../core/courses.php';
 $page_title = "Liste des Cours";
 include_once '../layout/header.php';
+include_once '../layout/alert.php';
 $courses = getAllCourses($mysqli);
 if (isset($_SESSION["message"])) {
-    echo "<div class='bg-green-100 border mb-2 border-green-400 text-green-700 px-4 py-3 rounded relative' role='alert'>
-            <strong class='font-bold'>Succès !</strong>
-            <span class='block sm:inline'> " . htmlspecialchars($_SESSION['message']) . "</span>
-        </div>";
+    alert_success($_SESSION["message"]);
     unset($_SESSION["message"]);
 }
 ?>
@@ -23,18 +21,6 @@ if (isset($_SESSION["message"])) {
             <div class="flex flex-row mb-1 sm:mb-0">
                 <div class="relative">
                     <select class="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                        <option>5</option>
-                        <option>10</option>
-                        <option>20</option>
-                    </select>
-                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                        </svg>
-                    </div>
-                </div>
-                <div class="relative">
-                    <select class="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
                         <option>All</option>
                         <option>Débutant</option>
                         <option>Intermédiaire</option>
