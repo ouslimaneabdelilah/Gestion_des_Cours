7caa49dff4f34ddc4339f5fa36e7d7ba230faa87
abdelilah ouslimane <abdelilah.ouslimane3740@gmail.com>
2025-12-09 11:44:37 +0100

    refactor: Improve code formatting and consistency in Course model methods

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -1,6 +1,7 @@
 <?php
 require_once "./app/Database/Database.php";
-class Course{
+class Course
+{
     private $pdo;
 
     public function __construct()
@@ -8,31 +9,47 @@ class Course{
         $db = new Database();
         $this->pdo = $db->getConnection();
     }
-    public function all(){
+    public function all()
+    {
         $stm = $this->pdo->prepare("SELECT * FROM courses");
         $stm->execute();
         return $stm->fetchAll();
     }
-    public function find($id){
+    public function find($id)
+    {
         $stm = $this->pdo->prepare("SELECT * FROM courses WHERE id = :id");
-        $stm->bindParam(":id",$id,PDO::PARAM_INT);
+        $stm->bindParam(":id", $id, PDO::PARAM_INT);
         $stm->execute();
         return $stm->fetch();
     }
-    public function create($title, $description, $level){
+    public function create($title, $description, $level)
+    {
         $stm = $this->pdo->prepare("INSERT INTO courses (title, description, level) VALUES (:title, :description, :level)");
-        $stm->bindParam(":title",$title,PDO::PARAM_STR);
-        $stm->bindParam(":description",$description,PDO::PARAM_STR);
-        $stm->bindParam(":level",$level,PDO::PARAM_STR);
+        $stm->bindParam(":title", $title, PDO::PARAM_STR);
+        $stm->bindParam(":description", $description, PDO::PARAM_STR);
+        $stm->bindParam(":level", $level, PDO::PARAM_STR);
         return $stm->execute();
     }
-    public function update($id,$title,$description,$level){
+    public function update($id, $title, $description, $level)
+    {
         $stm = $this->pdo->prepare("UPDATE courses SET title =:title, description=:description, level = :level WHERE id=:id");
-        $stm->bindParam(":id",$id,PDO::PARAM_STR);
-        $stm->bindParam(":title",$title,PDO::PARAM_STR);
-        $stm->bindParam(":description",$description,PDO::PARAM_STR);
-        $stm->bindParam(":level",$level,PDO::PARAM_STR);
+        $stm->bindParam(":id", $id, PDO::PARAM_STR);
+        $stm->bindParam(":title", $title, PDO::PARAM_STR);
+        $stm->bindParam(":description", $description, PDO::PARAM_STR);
+        $stm->bindParam(":level", $level, PDO::PARAM_STR);
         return $stm->execute();;
     }
+    public function delete($id)
+    {
+        $stm = $this->pdo->prepare("DELETE FROM courses WHERE id = :id");
+        $stm->bindParam(":id", $id, PDO::PARAM_INT);
+        return $stm->$stm->execute();;
+    }
+    public function getSections($courseId)
+    {
+        $stm = $this->pdo->prepare("SELECT * FROM sections WHERE course_id = :course_id");
+        $stm->bindParam(":course_id", $courseId, PDO::PARAM_INT);
+        $stm->execute();
+        return $stm->fetchAll();
+    }
 }
-?>
