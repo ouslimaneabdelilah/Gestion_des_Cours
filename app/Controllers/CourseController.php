e7c142a6c91eeae4d094ca31b6b360f66c95ea83
ouslimaneabdelilah <abdelilah.ouslimane@gmail.com>
2025-12-10 20:33:34 +0100

    refactor: Enhance course creation and update methods to handle image uploads and validation

--- app/Controllers/CourseController.php
+++ app/Controllers/CourseController.php
@@ -3,6 +3,8 @@ require_once "./app/Models/Course.php";
 class CourseController
 {
     private $courseModel;
+    private $levels = ["Débutant", "Intermédiaire", "Avancé"];
+
     public function __construct()
     {
         $this->courseModel = new Course();
@@ -12,6 +14,7 @@ class CourseController
     // affichier tout les course
     public function index()
     {
+
         $courses = $this->courseModel->all();
         include "./resources/views/courses/courses_list.php";
     }
@@ -20,37 +23,79 @@ class CourseController
     // envoyer a la page de create
     public function create()
     {
+
+        $levels = ["Débutant", "Intermédiaire", "Avancé"];
         include "./resources/views/courses/courses_create.php";
     }
 
     // ajouter un course 
-    public function store($data)
+    public function store()
     {
-        $this->courseModel->create($data["title"], $data["description"], $data["level"]);
-        header("Location:./resources/views/courses/courses_list.php");
+        print_r($_POST);
+        $levels = $this->levels;
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $title = trim($_POST["title"]);
+            $description = trim($_POST["description"]);
+            $level = trim($_POST["level"]);
+            if (empty($title) || empty($description) || empty($level)) {
+                $error_message = "Tous les champs sont requis.";
+                include "./resources/views/courses/courses_create.php";
+                return;
+            }
+
+            if (!in_array($level, $levels)) {
+                $error_message = "Level qui choisir n'est pas exect.";
+                include "./resources/views/courses/courses_create.php";
+                return;
+            }
+
+            $imageName = "image.png";
+
+            if (!empty($_FILES["image"]["name"])) {
+
+                $imageName = uniqid() . "_" . $_FILES["image"]["name"];
+                $destination = "./public/uploads/" . $imageName;
+
+                move_uploaded_file($_FILES["image"]["tmp_name"], $destination);
+            }
+
+
+
+            $this->courseModel->create($title, $description, $level, $imageName);
+            $_SESSION["message"] = "Le cours a été ajouté avec succès.";
+            header("Location: /courses");
+            exit();
+        }
     }
 
     // envoyer a la page d'edit
     public function edit($id)
     {
-        if (!isset($_GET['id']) || empty($_GET['id'])) {
+        if (!$id) {
             header("Location: /courses");
             exit();
         }
-        $id = $_GET['id'];
         $course = $this->courseModel->find($id);
-        $levels = ["Débutant", "Intermédiaire", "Avancé"];
+        if (!$course) {
+            $_SESSION['message'] = "Cours introuvable.";
+            header("Location: /courses");
+            exit();
+        }
+
+
+        $levels = $this->levels;
         include "./resources/views/courses/courses_edit.php";
     }
+
     // update un course
-    public function update($id, $data)
+    public function update($id)
     {
-        $levels = ["Débutant", "Intermédiaire", "Avancé"];
+        $course = $this->courseModel->find($id);
+        $levels = $this->levels;
         if ($_SERVER["REQUEST_METHOD"] !== "POST") {
             header("Location:/courses");
             exit();
         }
-        $id = $_POST["id"];
         $title = trim($_POST["title"]);
         $description = trim($_POST["description"]);
         $level = trim($_POST["level"]);
@@ -66,8 +111,17 @@ class CourseController
             include "./resources/views/courses/courses_edit.php";
             return;
         }
+        if (!empty($_FILES["image"]["name"])) {
 
-        $this->courseModel->update($id, $title, $description, $level);
+            $imageName = uniqid() . "_" . $_FILES["image"]["name"];
+            $destination = "./public/uploads/" . $imageName;
+
+            move_uploaded_file($_FILES["image"]["tmp_name"], $destination);
+        } else {
+            $imageName = $course["image"];
+        }
+
+        $this->courseModel->update($id, $title, $description, $level, $imageName);
         $_SESSION["message"] = "Modification réussie.";
         header("Location: /courses");
         exit;
