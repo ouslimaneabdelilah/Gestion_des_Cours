a80594f992d2e434d83aa92e7bbe64de506e01ad
abdelilah ouslimane <abdelilah.ouslimane3740@gmail.com>
2025-12-10 14:01:02 +0100

    fix: Correct database name typo in configuration file

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
 return [
     "host"=>"localhost",
-    "dbname"=>"gestion_des_cours",
+    "dbname"=>"gestions_des_coures",
     "username"=>"root",
     "password"=>"",
     "charset" => "utf8mb4"
