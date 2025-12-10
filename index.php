13407f8d0b633cf16eefe43aec450f4e1782198d
abdelilah ouslimane <abdelilah.ouslimane3740@gmail.com>
2025-12-10 14:01:36 +0100

    refactor: Remove unused User class and streamline index.php

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
-<?php 
+<?php
+session_start();
+require "./core/router.php";
+$router = new Router();
+require "./routes/web.php";
 
-class User{
-    public $name;
-    public function __construct($name)
-    {
-         $this->name = $name;
-    }
-    public function testHello(){
-        echo "Hello my name" . $this->name . "2025";
-    }
-
-}
-$user = new User("abdelilah");
-echo $user->testHello();
-echo "<pre>";
-print_r($user);
-echo "</pre>";
-?>
+$router->run();
