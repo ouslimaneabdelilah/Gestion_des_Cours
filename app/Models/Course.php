41a3a82fcb6526a117579a48da6be7176e910640
ouslimaneabdelilah <abdelilah.ouslimane@gmail.com>
2025-12-10 20:33:27 +0100

    refactor: Update create and update methods to include image parameter in Course model

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -22,20 +22,23 @@ class Course
         $stm->execute();
         return $stm->fetch();
     }
-    public function create($title, $description, $level)
+    public function create($title, $description, $level, $image)
     {
-        $stm = $this->pdo->prepare("INSERT INTO courses (title, description, level) VALUES (:title, :description, :level)");
+        $stm = $this->pdo->prepare("INSERT INTO courses (title, description, level,image) VALUES (:title, :description, :level,:image)");
         $stm->bindParam(":title", $title, PDO::PARAM_STR);
         $stm->bindParam(":description", $description, PDO::PARAM_STR);
         $stm->bindParam(":level", $level, PDO::PARAM_STR);
+        $stm->bindParam(":image", $image, PDO::PARAM_STR);
         return $stm->execute();
     }
-    public function update($id, $title, $description, $level)
+    public function update($id, $title, $description, $level, $image)
     {
-        $stm = $this->pdo->prepare("UPDATE courses SET title =:title, description=:description, level = :level WHERE id=:id");
+        $stm = $this->pdo->prepare("UPDATE courses SET title =:title, description=:description, level = :level, image= :image WHERE id=:id");
         $stm->bindParam(":id", $id, PDO::PARAM_STR);
         $stm->bindParam(":title", $title, PDO::PARAM_STR);
         $stm->bindParam(":description", $description, PDO::PARAM_STR);
+        $stm->bindParam(":image", $image, PDO::PARAM_STR);
+
         $stm->bindParam(":level", $level, PDO::PARAM_STR);
         return $stm->execute();;
     }
