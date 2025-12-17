65a2af7fbbd933861b24231365a2399f59d90866
ouslimaneabdelilah <abdelilah.ouslimane@gmail.com>
2025-12-17 15:02:23 +0100

    feat: add namespace

--- app/Database/Database.php
+++ app/Database/Database.php
@@ -1,4 +1,5 @@
 <?php 
+namespace App\Database;
 class Database{
     private $pdo;
     public function __construct()
@@ -6,14 +7,14 @@ class Database{
         $config = require "./config/config.php";
         $dsn = "mysql:host={$config['host']}; dbname={$config['dbname']};charset={$config['charset']}";
         $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES => false
         ];
 
         try {
-            $this->pdo = new PDO($dsn,$config["username"],$config["password"],$options);
-        } catch (PDOException $e) {
+            $this->pdo = new \PDO($dsn,$config["username"],$config["password"],$options);
+        } catch (\PDOException $e) {
             die("Error dans la connexion de database" . $e->getMessage());
         }
     }
