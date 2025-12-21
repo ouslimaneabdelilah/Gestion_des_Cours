8e6836315abecfc1efd70a488a978cee3fba85b4
ouslimaneabdelilah <abdelilah.ouslimane@gmail.com>
2025-12-21 19:24:38 +0100

    feat: add authentication routes for user registration, login, and logout

--- config/routes.php
+++ config/routes.php
@@ -1,11 +1,19 @@
 <?php
-// routes courses
 namespace Config;
+// routes auth
+
+$router->add('GET', 'register', 'AuthController@showRegister'); 
+$router->add('POST', 'register', 'AuthController@register');
+$router->add('GET', 'login', 'AuthController@showLogin');   
+$router->add('POST', 'login', 'AuthController@login'); 
+
+$router->add('GET', 'logout', 'AuthController@logout');
+// routes courses
 $router->add("GET","", 'CourseController@index');
 $router->add('GET','courses', 'CourseController@index');
 $router->add('GET','course/create', 'CourseController@create');
 $router->add('GET','course/{id}/edit', 'CourseController@edit');
-$router->add('GET','course/{id}/delete', 'CourseController@confirmDelete');
+$router->add('GET','course/{id}/confirme', 'CourseController@confirmDelete');
 $router->add('POST','course/store', 'CourseController@store');
 $router->add('GET','course/{id}/sectionsbycourse', 'CourseController@showSections');
 $router->add('POST','course/{id}/update', 'CourseController@update');
