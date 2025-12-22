0d98e1a585e58e0bf7dfb70eef9015c590c93fb9
ouslimaneabdelilah <abdelilah.ouslimane@gmail.com>
2025-12-22 21:53:37 +0100

    feat: add DashboardController for statistics management

---
+++ app/Controllers/DashboardController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Repositories\StatisticsRepository;
+
+class DashboardController
+{
+    private $statsRepo;
+
+    public function __construct(StatisticsRepository $statsRepo)
+    {
+        $this->statsRepo = $statsRepo;
+    }
+    public function index()
+    {
+        $totalCourses = $this->statsRepo->nombreTotaldeCours();
+        $totalUsers = $this->statsRepo->nombreUsers();
+        $totalEnrollments = $this->statsRepo->totalInsc();
+
+        $enrollmentsPerCourse = $this->statsRepo->inscriptionsParCours();
+
+        $popularCourse = $this->statsRepo->popularCourse();
+
+        $avgSections = $this->statsRepo->moyenneSectionsCours();
+
+        $heavyCourses = $this->statsRepo->coursRiche();
+
+        $usersThisYear = $this->statsRepo->inscriptionsCetteAnne();
+
+        $emptyCourses = $this->statsRepo->coursSansInscriptions();
+
+        $latestEnrollments = $this->statsRepo->dernieresInscriptions();
+        include "./resources/views/admin/stats_dashboard.php";
+    }
+}
